c6eb7bb13c709326fe27bd2fdcaf8029287d516c
tejas-warake <tswarake15@gmail.com>
2024-10-04 20:03:30 +0530

    overloaded new and delete to log allocation and deallocation

--- include/memory_tracker.hpp
+++ include/memory_tracker.hpp
@@ -1,4 +1,14 @@
 #ifndef MEMORY_TRACKER_HPP
 #define MEMORY_TRACKER_HPP
 
+#include <cstddef>
+
+void* operator new(size_t size, const char* file, int line);
+void* operator new[](size_t size, const char* file, int line);
+void operator delete(void* ptr) noexcept;
+void operator delete[](void* ptr) noexcept;
+void initializeMemoryTracker();
+void finalizeMemoryTracker();
+void reportLeaks();
+
 #endif
--- src/memory_tracker.cpp
+++ src/memory_tracker.cpp
@@ -1,1 +1,85 @@
 #include "memory_tracker.hpp"
+#include <iostream>
+#include <unordered_map>
+#include <cstdlib>
+#include <mutex>
+
+std::mutex memoryMutex;
+
+struct mem_alloc_struct {
+    size_t size;
+    const char* file;
+    int line;
+};
+
+std::unordered_map<void*, mem_alloc_struct> memAllocMap;
+
+void* operator new(size_t size, const char* file, int line) {
+    std::lock_guard<std::mutex> guard(memoryMutex);
+
+    void* ptr = std::malloc(size);
+    if (!ptr) throw std::bad_alloc();
+
+    memAllocMap[ptr] = {size, file, line};  // false means it's a scalar new
+    std::cout << "Allocated " << size << " bytes at " << ptr << " (File: " << file << ", Line: " << line << ")\n";
+    return ptr;
+}
+
+void* operator new[](size_t size, const char* file, int line) {
+    std::lock_guard<std::mutex> guard(memoryMutex);
+
+    void* ptr = std::malloc(size);
+    if (!ptr) throw std::bad_alloc();
+
+    memAllocMap[ptr] = {size, file, line};  
+    std::cout << "Allocated " << size << " bytes at " << ptr << " (File: " << file << ", Line: " << line << ")\n";
+    return ptr;
+}
+
+// Overload the global delete operator
+// void operator delete(void* ptr) noexcept {
+//     std::lock_guard<std::mutex> guard(memoryMutex);
+
+//     auto it = memAllocMap.find(ptr);
+//     if (it != memAllocMap.end()) {
+//         std::cout << "Deallocated " << it->second.size << " bytes from " << ptr << " (File: " << it->second.file << ", Line: " << it->second.line << ")\n";
+//         memAllocMap.erase(it);
+//     } else {
+//         std::cout << "Deallocated unknown memory " << ptr << "\n";
+//     }
+//     std::free(ptr);
+// }
+
+void operator delete[](void* ptr) noexcept {
+    std::lock_guard<std::mutex> guard(memoryMutex);
+
+    auto it = memAllocMap.find(ptr);
+    if (it != memAllocMap.end()) {
+        std::cout << "Deallocated " << it->second.size << " bytes from " << ptr << " (File: " << it->second.file << ", Line: " << it->second.line << ")\n";
+        memAllocMap.erase(it);
+    } else {
+        std::cout << "Deallocated unknown memory " << ptr << "\n";
+    }
+    std::free(ptr);
+}
+
+void reportLeaks() {
+    std::lock_guard<std::mutex> guard(memoryMutex);
+
+    if (!memAllocMap.empty()) {
+        std::cout << "Memory leaks detected:\n";
+        for (const auto& alloc : memAllocMap) {
+            std::cout << "Leaked " << alloc.second.size << " bytes at " << alloc.first 
+                      << " (File: " << alloc.second.file << ", Line: " << alloc.second.line << ")\n";
+        }
+    } else {
+        std::cout << "No memory leaks detected.\n";
+    }
+}
+
+void initializeMemoryTracker() {
+}
+
+void finalizeMemoryTracker() {
+    reportLeaks();
+}
